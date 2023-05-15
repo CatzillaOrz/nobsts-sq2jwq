e79698361f3f8acfd9e1fa6087ef43ff15f94e57
catzilla <catzillaorz@outlook.com>
2023-05-15 23:17:43 +0800

    feat: #8 Generic | Example in interfaces such as EventMap

--- generic-util-keyof-pluk/keyof-pluck.ts
+++ generic-util-keyof-pluk/keyof-pluck.ts
@@ -16,3 +16,30 @@ const dogs = [
 console.log(pluck(dogs, "name"));
 console.log(pluck(dogs, "age"));
 
+/**
+ * #8 Generic | Example in interfaces 
+ */
+interface BaseEvent {
+  time: number;
+  user: string;
+}
+
+interface EventMap {
+  addToCart: BaseEvent & { quantity: number; productId: string },
+  checkout: BaseEvent
+}
+
+function sendEvent<Name extends keyof EventMap>(
+  name: Name,
+  data: EventMap[Name]): void {
+  console.log([name, data]);
+}
+
+sendEvent("addToCart", {
+  productId: 'foo',
+  user: 'baz',
+  quantity: 1,
+  time: 10
+})
+
+sendEvent("checkout",{time:20, user: 'bob'} )
