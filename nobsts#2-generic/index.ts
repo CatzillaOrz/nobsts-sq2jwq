7ed0b5ee615ce7c63b2c24af03daab8bf1585382
catzilla <catzillaorz@outlook.com>
2023-05-15 22:24:37 +0800

    feat: #2 Generic | reduce -> foreach

---
+++ nobsts#2-generic/index.ts
@@ -0,0 +1,12 @@
+/**
+ * reduce -> foreach
+ */
+function myForEach<T>(items: T[], forEachFunc: (v: T) => void): void {
+  items.reduce((a, v) => {
+    forEachFunc(v);
+    return undefined;
+  }, undefined)
+}
+
+myForEach(["a", "b", "c"], (v) => console.log(`forEach ${v}`));
+
