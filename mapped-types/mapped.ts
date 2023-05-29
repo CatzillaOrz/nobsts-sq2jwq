e0aeb30761012587d59d9640ed5b9692c03f8f26
catzilla <catzillaorz@outlook.com>
2023-05-29 13:42:01 +0800

    feat: Mapped Types usage in Interface & Lister event function | API like

--- mapped-types/mapped.ts
+++ mapped-types/mapped.ts
@@ -23,3 +23,32 @@ type OptionsFlags<Type> = {
   [Property in keyof Type]: boolean;
 };
 type DoginfoOptions = OptionsFlags<DogInfo>
+
+/**
+ * Usage in Listeners function
+ * @Capitalize<Property> Utility
+ */
+
+type Listeners<Type> = {
+  [Property in keyof Type as  `on${Capitalize<string & Property>}Change`]?:
+  (newVal: Type[Property]) => void;
+} & {
+    [Property in keyof Type as  `on${Capitalize<string & Property>}Delete`]?:
+    (newVal: Type[Property]) => void;
+  }
+
+function listenToObject<T>(obj: T, listeners: Listeners<T>): void {
+  throw "Needs to be implemented";
+}
+
+const lg: DogInfo = {
+  name: "LG",
+  age: 12,
+};
+
+type DogInfoListeners = Listeners<DogInfo>;
+
+listenToObject(lg, {
+  onNameChange: (v: string) => { },
+  onAgeChange: (v: number) => { },
+});
