16d3becca82e8f23778aad62f7e21f9ebc060e14
catzilla <catzillaorz@outlook.com>
2023-07-03 18:25:59 +0800

    feat: visit-design-patter | iterator yield *

---
+++ Visiter-Design-Pattern/visitor-pattern-iterator.ts
@@ -0,0 +1,25 @@
+import fetch from 'node-fetch';
+// import { Pokemon } from './index'
+
+async function* interateResults<DataType>(
+  url: string
+) {
+  let nextUrl: string | undefined = url;
+  do {
+    const response = await fetch(nextUrl);
+    const json: {
+      next?: string;
+      results: DataType[];
+    } = await response.json();
+    yield json.results;
+    nextUrl = json.next;
+  } while (nextUrl);
+}
+
+(async function() {
+  for await (const result of interateResults(
+    'https://pokeapi.co/api/v2/pokemon',
+  )) {
+    console.log(result);
+  }
+})()
